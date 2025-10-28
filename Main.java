f1393d5a6cc54cafaf9331f742ac8a6e0ff98cc9
zhukovsamsung68-commits <zhukov.samsung68@gmail.com>
2025-10-28 23:04:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,156 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+        System.out.println(isSumBetween10And20(5, 10));
+        System.out.println(isSumBetween10And20(2, 3));
+
+        printSignOfNumber(5);
+        printSignOfNumber(-7);
+        printSignOfNumber(0);
+
+        System.out.println(isNegative(-1));
+        System.out.println(isNegative(0));
+
+        printWordNTimes("Астон", 3);
+
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(1900));
+        System.out.println(isLeapYear(2024));
+
+        flipArray();
+        fillArray();
+        multiplyIfLessThan6();
+        fillDiagonal();
+
+        int[] arr = createArray(5, 42);
+        printArray(arr);
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSign() {
+        int a = 5;
+        int b = -10;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = 10;
+        int b = 7;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    static boolean isSumBetween10And20(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    static void printSignOfNumber(int x) {
+        if (x >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    static boolean isNegative(int x) {
+        return x < 0;
+    }
+
+    static void printWordNTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
+        }
+    }
+
+    static boolean isLeapYear(int year) {
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        return year % 4 == 0;
+    }
+
+    static void flipArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        printArray(arr);
+    }
+
+    static void fillArray() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        printArray(arr);
+    }
+
+    static void multiplyIfLessThan6() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] = arr[i] * 2;
+        }
+        printArray(arr);
+    }
+
+    static void fillDiagonal() {
+        int n = 5;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            arr[i][i] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[] createArray(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
+
+    static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
